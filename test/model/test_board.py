b7d8fe5db46c67647802a09a5fc11a4c1843b7e3
Gekon <olekman13@gmail.com>
2019-12-01 20:15:45 +0100

    solvability for N*N boards
    Signed-off-by: Gekon <olekman13@gmail.com>

--- test/model/test_board.py
+++ test/model/test_board.py
@@ -10,6 +10,8 @@ class TestState(unittest.TestCase):
     cols = 2
     board1 = Board([Node(2), Node(1), Node(0), Node(3)], rows, cols)
     board2 = Board([Node(2), Node(1), Node(3), Node(0)], rows, cols)
+    board3 = Board([Node(1), Node(2), Node(5), Node(3), Node(4), Node(8), Node(6), Node(7), Node(0)], 3, 3)
+    board4 = Board([Node(0), Node(2), Node(1), Node(3), Node(4), Node(5), Node(6), Node(7), Node(8)], 3, 3)
 
     def test_solvable(self):
         # given
@@ -26,5 +28,25 @@ class TestState(unittest.TestCase):
 
         # when
         solvability = initial_state.is_solvable()
+        # then
+        self.assertEqual(solvability, False)
+
+    def test_solvable_3x3(self):
+        # given
+        initial_state = State(self.board3)
+
+        # when
+        solvability = initial_state.is_solvable()
+
+        # then
+        self.assertEqual(solvability, True)
+
+    def test_unsolvable_3x3(self):
+        # given
+        initial_state = State(self.board4)
+
+        # when
+        solvability = initial_state.is_solvable()
+
         # then
         self.assertEqual(solvability, False)
